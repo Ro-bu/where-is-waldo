a2b3dd6fb2ccd2893a6ae81a8ee80123d6718fe7
Ro-bu <robin.raid1@gmail.com>
2022-08-29 23:02:12 +0300

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <div className="header">
+            <p className="logo">
+                Where's Waldo?
+            </p>
+            <button type="button" className="leaderboard-button">See the leaderboard</button>
+        </div>
+    )
+}
+
+export default Header;
